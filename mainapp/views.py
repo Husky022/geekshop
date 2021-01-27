d1f59ae1125bc57f0e02321451aeb15b6c5e63e3
Ilya <balashovia@bk.ru>
2021-01-28 00:44:02 +0300

    Выполнены все задания, но не получилось добавить в изображения источник картинки с динамической папкой static: "img": "{% static 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'%}" (специально для наглядности оставил в одном товаре запись такого вида. Там не прогрузилась картинка) Прошу подсказать, в чем может быть косяк)

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,41 @@
 from django.shortcuts import render
+from datetime import datetime
+import json
+
+
+def get_products_data():
+    with open('./mainapp/fixtures/products.json', encoding='utf-8') as file:
+        products_data = json.load(file)
+        products = products_data['products']
+        return products
+
 
 # Create your views here.
 
 
+def base(request):
+    context = {
+        'now': datetime.now().year
+    }
+    return render(request, 'mainapp/base.html', context)
+
+
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'geekshop',
+        'now': datetime.now().year
+    }
+    return render(request, 'mainapp/index.html', context)
+
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'title': 'products',
+        'now': datetime.now().year,
+        'products': get_products_data()
+    }
+    return render(request, 'mainapp/products.html', context)
 
-#Lesson1
+
+
+
