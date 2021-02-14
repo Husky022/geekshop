3265376896ce5be264489ab0a9b1cf9ee83ffec5
Ilya <balashovia@bk.ru>
2021-02-14 20:17:48 +0300

    added condition for controller

--- adminapp/views.py
+++ adminapp/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth
 from authapp.models import User
-from  adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
+from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
+from django.contrib.auth.decorators import user_passes_test
 # Create your views here.
 
+
+@user_passes_test(lambda u:u.is_superuser, login_url='/')
 def index(request):
     return render(request, 'adminapp/index.html')
 
-
+@user_passes_test(lambda u:u.is_superuser)
 def admin_users_read(request):
     context = {'users': User.objects.all()}
     return render(request, 'adminapp/admin-users-read.html', context)
 
+@user_passes_test(lambda u:u.is_superuser)
 def admin_users_create(request):
     if request.method == 'POST':
         form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
@@ -26,6 +30,7 @@ def admin_users_create(request):
     context = {'form': form}
     return render(request, 'adminapp/admin-users-create.html', context)
 
+@user_passes_test(lambda u:u.is_superuser)
 def admin_users_update(request, id):
     user = User.objects.get(id=id)
     if request.method == 'POST':
@@ -38,6 +43,7 @@ def admin_users_update(request, id):
     context = {'form': form, 'current_user': user}
     return render(request,'adminapp/admin-users-update-delete.html', context)
 
+@user_passes_test(lambda u:u.is_superuser)
 def admin_users_delete(request, id):
     user = User.objects.get(id=id)
     user.is_active = False
