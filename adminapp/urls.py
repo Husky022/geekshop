d2649ba9488f4ba98fbf55edb6b5faad2c245a4b
Ilya <balashovia@bk.ru>
2021-02-17 23:00:59 +0300

    Added CBV

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -6,9 +6,9 @@ app_name = 'adminapp'
 
 urlpatterns = [
     path('', adminapp.index, name='index'),
-    path('admin-users-read/', adminapp.admin_users_read, name='admin_users_read'),
-    path('admin-users-create/', adminapp.admin_users_create, name='admin_users_create'),
-    path('admin-users-update/<int:id>', adminapp.admin_users_update, name='admin_users_update'),
-    path('admin-users-delete/<int:id>', adminapp.admin_users_delete, name='admin_users_delete'),
+    path('admin-users-read/', adminapp.UserListView.as_view(), name='admin_users_read'),
+    path('admin-users-create/', adminapp.UserCreateView.as_view(), name='admin_users_create'),
+    path('admin-users-update/<int:pk>', adminapp.UserUpdateView.as_view(), name='admin_users_update'),
+    path('admin-users-delete/<int:pk>', adminapp.UserDeleteView.as_view(), name='admin_users_delete'),
 
 ]
